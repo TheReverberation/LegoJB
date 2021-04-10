ab7d9dc1cbe635f7720cad932787578c5eb42ffa
Daniil <nedaiborsch.d@gmail.com>
2021-04-10 20:58:03 +0300

    Third

--- NonblockingMatcher.java
+++ NonblockingMatcher.java
@@ -4,14 +4,13 @@ import java.util.concurrent.*;
 public class NonblockingMatcher {
     public static void main(String[] args) {
         Future<Boolean> matchesFuture = new NonblockingMatcher().matches("abacaba.cpp", "a\\Sa\\Sa\\Sa\\.cpp");
-        while (!matchesFuture.isDone()) {
-            System.out.println("Wait for matches");
-        }
         try {
-            System.out.println(matchesFuture.get());
+            System.out.println(matchesFuture.get(200, TimeUnit.MILLISECONDS));
         } catch (Exception exc) {
             exc.printStackTrace();
         }
+        System.out.println("hello, world");
+
     }
 
     private ExecutorService executor = Executors.newSingleThreadExecutor();
